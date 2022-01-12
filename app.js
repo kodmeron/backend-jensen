cc6fe3898befe2f4ec6d3fa3cda49960a396e3b8
kodmeron <89447350+kodmeron@users.noreply.github.com>
2022-01-12 17:02:50 +0100

    Login formular tillagt.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const credentials = {secretUser:"user" , secretPassword:"password"}
 const cors = require("cors")
 const express = require("express")
 const app = express()
 
 app.use(cors())
 app.get("/", (req, res)=>{
+    const encodedAuth = (req.headers.authorization || '')
+    .split(' ')[1] || '' //getting the part after basic
+
+    const [name, password] = Buffer.from(encodedAuth, 'base64')
+    .toString().split(':')
+
+    console.log(`Name ${name}`)
+    console.log(`Password ${password}`)
+
     res.set('WWW-Authenticate', 'Basic realm="Access to Index"')
     res.status(401).send("Unauthorized access")
 })
